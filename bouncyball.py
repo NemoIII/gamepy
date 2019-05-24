5a9d052987e06a01e83a2fd9587a173161345673
NemoIII <pablo.iii@outlook.com>
2019-05-24 16:48:54 -0300

    d

---
+++ bouncyball.py
@@ -0,0 +1,11 @@
+from random import *
+from turtle import *
+from base import vector
+
+
+def value():
+	#this will generate me value from [-5, -3] to [3, 5]
+	return (3 + random() * 2) * choice([-1, 1])
+
+	
+	

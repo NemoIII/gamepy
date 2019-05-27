ae6c79b079d9f022b3388e74e8e551f4ff7a2700
NemoIII <pablo.iii@outlook.com>
2019-05-27 15:13:02 -0300

    changes

--- bouncyball.py
+++ bouncyball.py
@@ -7,4 +7,10 @@ def value():
 	#this will generate me value from [-5, -3] to [3, 5]
 	return (3 + random() * 2) * choice([-1, 1])
 
-	
+ball = vector(0, 0)
+aim = vector(value(),value())
+
+#the projection or de movement
+
+def draw():
+	pass

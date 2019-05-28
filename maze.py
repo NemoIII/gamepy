26eda420d8f01018385b8df1fbe06271382b2c2f
NemoIII <pablo.iii@outlook.com>
2019-05-28 11:19:37 -0300

    up

--- maze.py
+++ maze.py
@@ -3,3 +3,15 @@ from turtle import *
 from base import line
 
 
+def draw():
+    color('black')
+    width(5)
+    for x in range(-200, 200, 40):
+        for y in range(-200, 200, 40):
+            if random()>0.5:
+                line(x, y, x+40, y+40)
+            else:
+                line(x, y+40, x+40, y)
+
+
+''' Parei no meio do vídeo, continuar depois'''
